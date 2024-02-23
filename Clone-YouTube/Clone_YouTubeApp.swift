19cf24216940dd29fc29e865d3d3d7ec3b394e81
Kevin Hernandez <kmhs-18@hotmail.com>
2024-02-23 01:43:07 -0300

    Initial Commit

---
+++ Clone-YouTube/Clone_YouTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clone_YouTubeApp.swift
+//  Clone-YouTube
+//
+//  Created by Kevin Hernandez on 23/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Clone_YouTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
